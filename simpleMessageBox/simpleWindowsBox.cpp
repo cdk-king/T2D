853f0d95d774bc012efec36f24ab3b1c18f9e382
cdk-king <1360531956@qq.com>
2019-04-24 20:28:39 +0800

    添加第一个Window程序simpleMessageBox

---
+++ simpleMessageBox/simpleWindowsBox.cpp
@@ -0,0 +1,20 @@
+#define WIN32_LEAN_AND_MEAN
+
+#include <windows.h>		//the main windows headers
+#include <windowsx.h>		//a lot of cool macros
+
+//main entry point for all windows programs
+int WINAPI WinMain(
+HINSTANCE hinstance,
+HINSTANCE hprevinstance,
+LPSTR lpcmdline,
+int ncmdshow
+) {
+	//call message box api with NULL for parent window handle
+	MessageBox(NULL,"CDK_KING!!!","MY FIRST WINDOWS PROGRAM",MB_OK | MB_ICONEXCLAMATION);
+	//ICON EXCLAMATIONÍ¼±ê¸ÐÌ¾ºÅ
+
+	return (0);
+}	//end WinMain
+
+
